fa3ff3e70e52803b8c27dcb0e3e2e630fa9763cb
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2019-10-23 10:01:42 +0200

    use values.yaml for helm

--- apply.sh
+++ apply.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-NETWORK_TYPE="calico"
-PLATEFORM_NAME="calico"
+NETWORK_TYPE="cilium"
+PLATEFORM_NAME="cilium"
 
 cd terraform/layer-base
 terraform workspace new $PLATEFORM_NAME
-terraform apply
+terraform apply -auto-approve
 cd -
 
 cd terraform/layer-bastion
 terraform workspace new $PLATEFORM_NAME
-terraform apply
+terraform apply -auto-approve
 cd -
 
 cd terraform/layer-eks
 terraform workspace new $PLATEFORM_NAME
-terraform apply
+terraform apply -auto-approve
 terraform output kubeconfig > ../../tmp/.kubeconfig_$PLATEFORM_NAME
 terraform output config_map_aws_auth > ../../tmp/cm_auth_$PLATEFORM_NAME.yaml
 K8S_ENDPOINT=$(terraform output k8s_endpoint)
@@ -26,10 +26,11 @@ helm3 repo add stable https://kubernetes-charts.storage.googleapis.com/
 # ssh ec2-user@bastion.aws-wescale.slavayssiere.fr -L 8443:${K8S_ENDPOINT:8}:443 &
 ssh -M -S my-ctrl-socket -fnNT -L 8443:k8s-master.slavayssiere.wescale:443 ec2-user@bastion.$PLATEFORM_NAME.aws-wescale.slavayssiere.fr
 
+export KUBECONFIG="./tmp/.kubeconfig_$PLATEFORM_NAME"
 
 if [ "$NETWORK_TYPE" == "calico" ]; then
     echo "Installation de calico"
-    KUBECONFIG="./tmp/.kubeconfig_$PLATEFORM_NAME" kubectl apply -f https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/release-1.6/config/v1.5/calico.yaml
+    kubectl apply -f https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/release-1.6/config/v1.5/calico.yaml
 else
     echo "Installation de cilium"
     if [ ! -d "cilium-v1.6.3" ]; then
@@ -38,23 +39,20 @@ else
         rm -f cilium.tar.gz
     fi
 
-    KUBECONFIG="./tmp/.kubeconfig_$PLATEFORM_NAME" helm3 install \
+    helm3 install \
         --namespace kube-system \
-        --set global.cni.chainingMode=aws-cni \
-        --set global.masquerade=false \
-        --set global.tunnel=disabled \
-        --set global.nodeinit.enabled=true \
+        --values ./helm_values/cilium.yaml \
         cilium cilium-v1.6.3/install/kubernetes/cilium
 fi
 
-KUBECONFIG="./tmp/.kubeconfig_$PLATEFORM_NAME" kubectl apply -f ./tmp/cm_auth_$PLATEFORM_NAME.yaml
-KUBECONFIG="./tmp/.kubeconfig_$PLATEFORM_NAME" helm3 install \
-    --set rbac.enabled=true,dashboard.enabled=true,metrics.prometheus.enabled=false,metrics.serviceMonitor.enabled=true,serviceType=NodePort,service.nodePorts.http=32001,kubernetes.ingressClass=public-ingress \
+kubectl apply -f ./tmp/cm_auth_$PLATEFORM_NAME.yaml
+helm3 install \
     --namespace kube-system \
+    --values ./helm_values/traefik-public.yaml \
     public-ingress stable/traefik
-KUBECONFIG="./tmp/.kubeconfig_$PLATEFORM_NAME" helm3 install \
-    --set rbac.enabled=true,dashboard.enabled=true,metrics.prometheus.enabled=false,metrics.serviceMonitor.enabled=true,serviceType=NodePort,service.nodePorts.http=32002,kubernetes.ingressClass=private-ingress \
+helm3 install \
     --namespace kube-system \
+    --values ./helm_values/traefik-private.yaml \
     private-ingress stable/traefik
 
 ssh -S my-ctrl-socket -O exit ec2-user@bastion.$PLATEFORM_NAME.aws-wescale.slavayssiere.fr
@@ -62,7 +60,7 @@ ssh -S my-ctrl-socket -O exit ec2-user@bastion.$PLATEFORM_NAME.aws-wescale.slava
 
 cd terraform/layer-alb
 terraform workspace new $PLATEFORM_NAME
-terraform apply
+terraform apply -auto-approve
 cd -
 
 # you can use aws eks --region eu-west-1 update-kubeconfig --name eks-test too
