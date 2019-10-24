0b0d2b754ef6300ea3c96813c733bfbdabc0f4d2
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2019-10-24 10:01:46 +0200

    add cilium monitoring

--- apply.sh
+++ apply.sh
@@ -60,6 +60,14 @@ else
         --namespace kube-system \
         --values ./helm_values/cilium.yaml \
         cilium cilium-v1.6.3/install/kubernetes/cilium
+
+    # la création des SM dans cilium est désactivé
+    kubectl apply -f ./mon-cilium/prometheus.yaml
+    kubectl apply -f ./mon-cilium/agent-sm.yaml
+    kubectl apply -f ./mon-cilium/operator-sm.yaml
+    kubectl apply -f ./mon-cilium/grafana-datasource.yaml
+    kubectl apply -f ./mon-cilium/grafana-dashboard.yaml
+    
 fi
 
 # installation du prometheus operator
@@ -67,6 +75,7 @@ helm3 install \
     --namespace observability \
     --values ./helm_values/prometheus-operator.yaml \
     --version 6.21.0 \
+    --wait \
     prometheus-operator stable/prometheus-operator
 
 # installation des IngressController
@@ -74,11 +83,13 @@ helm3 install \
     --namespace kube-system \
     --values ./helm_values/traefik-public.yaml \
     --set dashboard.domain=private.$PLATEFORM_NAME.aws-wescale.slavayssiere.fr \
+    --wait \
     public-ingress stable/traefik
 helm3 install \
     --namespace kube-system \
     --values ./helm_values/traefik-private.yaml \
     --set dashboard.domain=private.$PLATEFORM_NAME.aws-wescale.slavayssiere.fr \
+    --wait \
     private-ingress stable/traefik
 
 # add ingress
