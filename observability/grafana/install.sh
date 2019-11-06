12e01ef0e699015d768d5f3608d0be5d7f64cd74
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2019-11-06 16:35:01 +0100

    remove old way

--- observability/grafana/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-yum install -y https://dl.grafana.com/oss/release/grafana-6.4.3-1.x86_64.rpm
-systemctl daemon-reload
-systemctl start grafana-server
-systemctl enable grafana-server.service
