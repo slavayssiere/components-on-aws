a4f5118fab4cb21960a2887f5959e4a9bc7b70fc
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2019-11-08 11:21:09 +0100

    change init and make destroy executable

--- init.sh
+++ init.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-rm -Rf terraform/component_base/.terraform
-rm -Rf terraform/component_network/.terraform
-rm -Rf terraform/component_bastion/.terraform
-rm -Rf terraform/component_eks/component_alb/.terraform
-rm -Rf terraform/component_eks/.terraform
+# rm -Rf terraform/component_base/.terraform
+# rm -Rf terraform/component_network/.terraform
+# rm -Rf terraform/component_bastion/.terraform
+# rm -Rf terraform/component_eks/component_alb/.terraform
+# rm -Rf terraform/component_eks/.terraform
+# rm -Rf terraform/component_web/.terraform
 
 cd terraform/component_base
-terraform init
-cd -
-
-cd terraform/component_network
 terraform init
 cd -
 
@@ -22,6 +19,18 @@ cd terraform/component_eks
 terraform init
 cd -
 
-cd terraform/component_eks/component_alb
+cd terraform/component_eks/component-alb
+terraform init
+cd -
+
+cd terraform/component_network
+terraform init
+cd -
+
+cd terraform/component_rds
+terraform init
+cd -
+
+cd terraform/component_web
 terraform init
 cd -
