63c4ff85be3ee933f056ba0ecd386269b5290df0
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2019-11-26 11:32:15 +0100

    remove cognito

--- terraform/component_base/cognito.tf
+++ terraform/component_base/cognito.tf
@@ -1,50 +1,50 @@
-resource "aws_cognito_user_pool" "user-pool" {
-  name = "pool-${terraform.workspace}"
+// resource "aws_cognito_user_pool" "user-pool" {
+//   name = "pool-${terraform.workspace}"
 
-  alias_attributes          = ["email"]
-  auto_verified_attributes  = ["email"]
-  mfa_configuration         = "OFF"
-}
+//   alias_attributes          = ["email"]
+//   auto_verified_attributes  = ["email"]
+//   mfa_configuration         = "OFF"
+// }
 
-resource "aws_cognito_user_pool_client" "client" {
-  name = "client-pool-${terraform.workspace}"
+// resource "aws_cognito_user_pool_client" "client" {
+//   name = "client-pool-${terraform.workspace}"
 
-  generate_secret     = true
-  allowed_oauth_flows = ["code"]
-  allowed_oauth_flows_user_pool_client = true
-  allowed_oauth_scopes = ["phone", "email", "openid", "profile", "aws.cognito.signin.user.admin"]
+//   generate_secret     = true
+//   allowed_oauth_flows = ["code"]
+//   allowed_oauth_flows_user_pool_client = true
+//   allowed_oauth_scopes = ["phone", "email", "openid", "profile", "aws.cognito.signin.user.admin"]
 
   
-  read_attributes                         = ["email", "profile"]
-  write_attributes                        = ["email", "profile"]
+//   read_attributes                         = ["email", "profile"]
+//   write_attributes                        = ["email", "profile"]
 
-  supported_identity_providers            = ["COGNITO"]
+//   supported_identity_providers            = ["COGNITO"]
 
-  callback_urls = [
-    "https://grafana.${terraform.workspace}.${var.public_dns}/oauth2/callback",
-    "https://tracing.${terraform.workspace}.${var.public_dns}/oauth2/callback"
-  ]
+//   callback_urls = [
+//     "https://grafana.${terraform.workspace}.${var.public_dns}/oauth2/callback",
+//     "https://tracing.${terraform.workspace}.${var.public_dns}/oauth2/callback"
+//   ]
 
-  user_pool_id = "${aws_cognito_user_pool.user-pool.id}"
-}
+//   user_pool_id = "${aws_cognito_user_pool.user-pool.id}"
+// }
 
-resource "aws_cognito_user_pool_domain" "domain" {
-  domain          = "domain-${terraform.workspace}"
-  user_pool_id    = "${aws_cognito_user_pool.user-pool.id}"
-}
+// resource "aws_cognito_user_pool_domain" "domain" {
+//   domain          = "domain-${terraform.workspace}"
+//   user_pool_id    = "${aws_cognito_user_pool.user-pool.id}"
+// }
 
-output "user_pool_arn" {
-    value = "${aws_cognito_user_pool.user-pool.arn}"
-}
+// output "user_pool_arn" {
+//     value = "${aws_cognito_user_pool.user-pool.arn}"
+// }
 
-output "user_pool_client_id" {
-    value = "${aws_cognito_user_pool_client.client.id}"
-}
+// output "user_pool_client_id" {
+//     value = "${aws_cognito_user_pool_client.client.id}"
+// }
 
-output "user_pool_client_secret" {
-    value = "${aws_cognito_user_pool_client.client.client_secret}"
-}
+// output "user_pool_client_secret" {
+//     value = "${aws_cognito_user_pool_client.client.client_secret}"
+// }
 
-output "user_pool_domain" {
-    value = "${aws_cognito_user_pool_domain.domain.domain}"
-} 
+// output "user_pool_domain" {
+//     value = "${aws_cognito_user_pool_domain.domain.domain}"
+// } 
